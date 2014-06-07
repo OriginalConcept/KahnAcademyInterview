335a6d52bfefb6d991445592b93a958a6c82a831
OriginalConcept <adf1993@gmail.com>
2014-06-06 18:58:55 -0700

    first commit

---
+++ conditions.js
@@ -0,0 +1,60 @@
+
+
+function TestCase(text)					//TestCase class has text and passed parameters
+{
+	this.text = text;
+	this.passed = false;
+}
+
+var testCases = [];						//three example test cases
+var case0 = new TestCase('Declare a variable.');
+testCases.push(case0);
+
+var case1 = new TestCase('Write an if statement.');
+testCases.push(case1);
+
+var case2 = new TestCase('Do not use a while loop.');
+testCases.push(case2);
+
+
+function validateCases(input){
+	
+	var syntax = esprima.tokenize(input);
+
+	//These cases could be combined.  Seperate for clarity.  
+
+	//Test Case 0
+	testCases[0].passed = false;
+	for (var i = 0; i < syntax.length; i++)
+ 	{
+   		if (syntax[i].type == "Keyword" && syntax[i].value == "var")
+	    {
+	     testCases[0].passed = true;
+	    }
+  	}
+
+  	//Test Case 1
+	testCases[1].passed = false;
+	for (i = 0; i < syntax.length; i++)
+ 	{
+    if (syntax[i].type == "Keyword" && syntax[i].value == "if")
+	    {
+	      testCases[1].passed = true;
+	    }
+  	}
+
+  	//Test Case 2
+	testCases[2].passed = true;
+	for (i = 0; i < syntax.length; i++)
+ 	{
+    if (syntax[i].type == "Keyword" && syntax[i].value == "while")
+	    {
+	      testCases[2].passed = false;
+	    }
+  	}
+	
+	return testCases;
+	
+	//return testCases;
+}
+
