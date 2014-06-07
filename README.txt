303bae18cf785c2f8f0da8499288e47a959c34a8
OriginalConcept <adf1993@gmail.com>
2014-06-06 20:22:32 -0700

    fixed some formatting

--- README.txt
+++ README.txt
@@ -2,13 +2,17 @@ Hey!  Thanks for checking out my project.
 
 Here’s a little bit about it.
 
-editor.html contains the editor and conditions.js is holds the conditions to be checked.
+I used the code editor Ace along with Esprima to check if certain conditions were fulfilled by the code entered in the text box.
+
+editor.html contains the editor and conditions.js is holds the conditions to be cleared.
 
 1. 
 Acorn.js vs Esprima:
 I chose to use Esprima on the basis of superior documentation and support.  Esprima has a speed comparison page (http://esprima.org/test/compare.html).  Esprima is around 50% slower than Acorn.js but the absolute difference should not be relevant.
 
 ‘“Acorn.js” tutorial' turned up about 8,000 results on google, while '”Esprima” tutorial' produced about 32,000 results.  This is more important to me.
+
+I was unsatisfied with the documentation for Esprima.  If I were to continue development, I would give a deeper look into Acorn.js.
 
 2.
 I could not test this program on Internet Explorer but everything is fully functional on Firefox, Chrome, and Safari.
@@ -21,6 +25,8 @@ Ace is amazingly nice.  It made this project a lot easier.
 5.
 There’s no slowdown to user input. 
 
+Known issues:
+Creating an open ended string as the first line of code will cause this to break.  It’s a problem with Ace.
 
 
 By Adam Fisher
--- conditions.js
+++ conditions.js
@@ -7,51 +7,51 @@ function TestCase(text)					//TestCase class has text and passed parameters
 }
 
 var testCases = [];						//three example test cases
-var case0 = new TestCase('Declare a variable.');
+var case0 = new TestCase('Declare a variable.  Try typing: var a = 1;');
 testCases.push(case0);
 
-var case1 = new TestCase('Write an if statement.');
+var case1 = new TestCase('Write an if statement. Before the console.log statement, try typing: if (a == 1)')
 testCases.push(case1);
 
-var case2 = new TestCase('Do not use a while loop.');
+var case2 = new TestCase('Do not use a while loop.  Do not even think about typing while.');
 testCases.push(case2);
 
 
 function validateCases(input){
 	
-	var syntax = esprima.tokenize(input);
+	var syntax = esprima.tokenize(input, { tolerant: true } );
 
 	//These cases could be combined.  Seperate for clarity.  
 
 	//Test Case 0
 	testCases[0].passed = false;
 	for (var i = 0; i < syntax.length; i++)
- 	{
-   		if (syntax[i].type == "Keyword" && syntax[i].value == "var")
-	    {
-	     testCases[0].passed = true;
-	    }
-  	}
+	{
+		if (syntax[i].type == "Keyword" && syntax[i].value == "var")
+		{
+		 testCases[0].passed = true;
+		}
+	}
 
-  	//Test Case 1
+	//Test Case 1
 	testCases[1].passed = false;
 	for (i = 0; i < syntax.length; i++)
- 	{
-    if (syntax[i].type == "Keyword" && syntax[i].value == "if")
-	    {
-	      testCases[1].passed = true;
-	    }
-  	}
+	{
+	if (syntax[i].type == "Keyword" && syntax[i].value == "if")
+		{
+		  testCases[1].passed = true;
+		}
+	}
 
-  	//Test Case 2
+	//Test Case 2
 	testCases[2].passed = true;
 	for (i = 0; i < syntax.length; i++)
- 	{
-    if (syntax[i].type == "Keyword" && syntax[i].value == "while")
-	    {
-	      testCases[2].passed = false;
-	    }
-  	}
+	{
+	if (syntax[i].type == "Keyword" && syntax[i].value == "while")
+		{
+		  testCases[2].passed = false;
+		}
+	}
 	
 	return testCases;
 	
